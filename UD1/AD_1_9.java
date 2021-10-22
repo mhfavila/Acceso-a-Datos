862e2ff7f8814cdd73455325cdb8c69ae8d1c9ff
mhfavila <61873992+mhfavila@users.noreply.github.com>
2021-10-22 16:29:57 +0200

    Actualizacion actividad 9

--- UD1/AD_1_9.java
+++ UD1/AD_1_9.java
@@ -11,33 +11,43 @@ import java.util.Scanner;
  * @author Favila
  *
  */
-/*A partir del primer ejemplo en los apuntes de lectura de ficheros de texto, escribe un programa en
-java que haga lo lea caracteres de X en X, siendo X un número entre 4 y 10 introducido por el
-usuario (para almacenar las lecturas de X caracteres tendréis que crear un array de tamaño X y del
-tipo correspondiente para guardar caracteres).
- * */
+/*
+ * A partir del primer ejemplo en los apuntes de lectura de ficheros de texto,
+ * escribe un programa en java que haga lo lea caracteres de X en X, siendo X un
+ * número entre 4 y 10 introducido por el usuario (para almacenar las lecturas
+ * de X caracteres tendréis que crear un array de tamaño X y del tipo
+ * correspondiente para guardar caracteres).
+ */
 public class AD_1_9 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		//********************************REVISAR NO FUNCIONA BIEN
-		System.out.println("Cuantos caracteres quieres leer :");
-		int caracter=0;
-		Scanner sc = new Scanner(System.in);
-		caracter= sc.nextInt();
-		char[] caracteres = null;
-		String ruta = "C:\\Users\\ALUMNO\\Documents\\GitHub\\2DAM-REP\\ACCESO A DATOS\\T_1\\src\\fichero.txt";
-		//String ruta = "C:\\Users\\Favila\\Documents\\GitHub\\2DAM-REP\\ACCESO A DATOS\\T_1\\src\\fichero.txt";
-		File fichero = new File(ruta);
-		FileReader fic = new FileReader(fichero);
-		
-		int i;
-		while((i = fic.read()) !=-1)
-			System.out.println(i);
-			//System.out.print((char) i);
-		fic.close();
-		
 
+		File fichero = new File("C:\\Users\\ALUMNO\\Documents\\GitHub\\2DAM-REP\\ACCESO A DATOS\\T_1\\src\\fichero.txt");
+		FileReader fic = new FileReader(fichero);
+		int i;
+		int x;
+		int contador = 0;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Introduce el numero de caracteres: ");
+		x = sc.nextInt();
+
+		char[] c = new char[x];//guardamos en un array el valor de los caracteres
+
+		while ((i = fic.read()) != -1) {//leemos el archivo 
+			if (contador == x) {
+				//cuando el contador sea igual a la los caracteres se sale del bucle
+				break;
+			}
+			fic.skip(x);//se saltan los carracteres 
+			c[contador] = (char) i;
+			contador++;
+		}
+
+		fic.close();
+		System.out.print(c);
 	}
 
 }
